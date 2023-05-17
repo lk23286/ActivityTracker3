687e211c075002258077d6306dcda06d04e2c658
lk23286 <lk23286@gmail.com>
2023-05-17 17:20:19 +0200

    Initial Commit

---
+++ ActivityTracker3/ActivityTracker3App.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityTracker3App.swift
+//  ActivityTracker3
+//
+//  Created by Laszlo Kovacs on 2023. 05. 17..
+//
+
+import SwiftUI
+
+@main
+struct ActivityTracker3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
