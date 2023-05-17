3b7fd9a9a2b8e81a20de48e9102efe9c0f0c3589
lk23286 <lk23286@gmail.com>
2023-05-17 18:00:09 +0200

    start

---
+++ ActivityTracker3/Models/Activities.swift
@@ -0,0 +1,27 @@
+//
+//  Activities.swift
+//  ActivityTracker3
+//
+//  Created by Laszlo Kovacs on 2023. 05. 17..
+//
+
+import Foundation
+struct Activities {
+    
+}
+
+struct Activity {
+    
+}
+
+struct SubActivity {
+    
+}
+
+struct Theme {
+    
+}
+
+struct HourMin {
+    
+}
